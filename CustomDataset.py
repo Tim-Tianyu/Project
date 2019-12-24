b98b5001a911162db6a8cccdf8d244b226a4bca5
Tianyu Wang <1463026393@qq.com>
2019-12-24 23:21:29 +0000

    implement rescaled dataset

--- CustomDataset.py
+++ CustomDataset.py
@@ -21,7 +21,8 @@ Distribution_names = ["linear_unbalanced_1",
          "expo_unbalance_2560to5", 
          "expo_unbalance_5000to1",
          "cut_unbalance_4890or25", 
-         "linear_unbalance_1020to3"]
+         "linear_unbalance_1020to3",
+         "2560to5_rescaled"]
 Datasets_name = ["MNIST", "CIFAR10"]
 
 class CustomCIFAR10(datasets.CIFAR10):
@@ -40,6 +41,14 @@ class CustomMNIST(datasets.MNIST):
         if (train):
             self.data = self.data[result_idxs,:,:]
             self.targets = self.targets[result_idxs]
+            
+# class CustomMNIST_rescale(CustomMNIST):
+#     def __init__(self, root, train=True, transform=MNIST_transform, target_transform=None, download=False, indexs_name = "index_test.npy", rescaled_index_name = "XXX.npy"):
+#         super().__init__(root, train, transform, target_transform, download, indexs_name)
+#         rescaled_idxs = np.load(root+'/'+rescaled_index_name)
+#         if (train):
+#             self.data = self.data[rescaled_idxs,:,:]
+#             self.targets = list(np.array(self.targets)[rescaled_idxs])
 
 class DataSetNotFound(Exception):
     pass
@@ -81,6 +90,19 @@ def random_select(idx_dic, num_list = [5000]*10, randomState=np.random.RandomSta
         randomState.shuffle(idxs_for_i)
         idxs = idxs + idxs_for_i[0:num_list[i]]
     return np.sort(np.array(idxs))
+
+def duplicate_idxs(idx_dic, target_num, randomState=np.random.RandomState(np.random.seed(12345))):
+    idxs = []
+    isList = isinstance(target_num,list)
+    for (k,v) in idx_dic.items():
+        if isList:
+            number = target_num[k]
+        else:
+            number = target_num    
+        idxs = idxs + int(number / len(v)) * v
+        idxs = idxs + v[0: (number%len(v))]
+    randomState.shuffle(idxs)
+    return idxs
 
 def produce_idx_dic(targets):
     # initialize the index dic
